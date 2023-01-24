ca6a3a49c81815353d86eba12711b89b6f49403d
Алексей Бабкин <is53-babkinas@radiotech.su>
2023-01-24 18:13:28 +0500

    Создана стартовая страница

---
+++ index.php
@@ -0,0 +1,68 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Сортировочная</title>
+</head>
+<body>
+<logo>
+    <h>AkiTea projects</h>
+</logo>
+<header>
+    Choose your champion!
+</header>
+<div class="buttons">
+    <a href="aapanel">Панель управления</a>
+    <a href="PHPADMIN">PHP My Admin</a>
+    <a href="https://akitea.xyz">Сайт курсовой работы</a>
+</div>
+<div class="buttons">
+    <a href="ftp">FTP</a>
+</div>
+</body>
+</html>
+<style>
+    logo{
+        font-size: 32px;
+        font-family: Forte;
+        color: #ea3434;
+        position: fixed;
+        margin-left: 40px;
+    }
+    header{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        font-size: 40px;
+        font-family: Forte;
+        font-weight: bolder;
+        margin-top: 40px;
+        color: #38919d;
+    }
+
+    .buttons{
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        margin-top: 40px;
+        gap: 40px;
+    }
+    .buttons a{
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        background-color: lightblue;
+        border: none;
+        height: 70px;
+        width: 300px;
+        font-size: 24px;
+        border-radius: 20px;
+        color: #2d7181;
+        text-decoration: none;
+        font-family: sans-serif;
+    }
+</style>
+<?php
