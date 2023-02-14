523be920c31e348fd6f383c83a7355deebf0efb3
AkiTea <outcast.pathetic@vk.com>
2023-02-15 00:23:55 +0500

    asasdasdas

--- index.php
+++ index.php
@@ -6,13 +6,27 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-    <link rel="stylesheet" href="./style.css">
+    <link rel="stylesheet" href="./styles/style.css">
+    <link rel="stylesheet" href="./styles/header.css">
+    <script src=""></script>
 </head>
 <body>
 <header>
-    <div class="h_s1"><img src="./imgs/logo.png" alt="" width="100px"></div>
-    <div class="h_s2"></div>
-    <div class="h_s3"></div>
+    <div class="h_s1">
+        <img src="./imgs/hamb.png" alt="" class="hambBtn">
+        <img src="./imgs/logo.png" alt="" class="headLogo">
+    </div>
+    <div class="burgar">
+        <div class="burgList">
+        <a href="">Главная</a>
+        <a href="">Фильмы</a>
+        <a href="">Сериалы</a>
+        <a href="">Профиль</a>
+        <a href="">Приобретенное</a>
+        </div>
+    </div>
+        <div class="h_s2"><input type="search" name="" id=""></div>
+        <div class="h_s3"></div>
 </header>
 <wrapper>
 
