9ec36833d4075f376f1f8b6ea2492d9f42f0fe98
akitea <outcast.pathetic@vk.com>
2023-03-01 10:53:24 +0500

    User profile cabinet is done

--- cabinet.php
+++ cabinet.php
@@ -36,85 +36,99 @@
     </div>
 </header>
 <wrapper>
-<div class="cab__left">
-    <div class="avatar">
-        <img src="" alt="">
-        <label>
-        <input type="file" id="profile_pic" name="profile_pic"
-               accept=".jpg, .jpeg, .png">
-        </label>
-    </div>
-    <div class="cab__warn">
-        <h2>Удалить профиль</h2>
-        <h2>Изменить логин</h2>
-    </div>
-</div>
-    <div class="cab__mid">
-        <div class="mid__inputs">
-            <div class="cab__name">
-                <h2>Имя:</h2>
-                <input type="text">
+    <div class="cabinet__top">
+        <div class="cab__left">
+            <div class="avatar">
+                <img src="./imgs/user.png" alt="">
+                <label class="input-file">
+                    <input type="file" name="file">
+                    <span>Выберите файл</span>
+                </label>
             </div>
-
-            <div class="cab__surname">
-                <h2>Фамилия:</h2>
-                <input type="text">
+            <div class="cab__warn">
+                <a>Удалить профиль</a>
+                <a>Изменить логин</a>
             </div>
+        </div>
+            <div class="cab__mid">
+                <div class="mid__inputs">
+                    <div class="cab__name">
+                        <h2>Имя:</h2>
+                        <input type="text">
+                    </div>
 
-            <div class="cab__site">
-                <h2>Сайт:</h2>
-                <input type="text">
+                    <div class="cab__surname">
+                        <h2>Фамилия:</h2>
+                        <input type="text">
+                    </div>
+
+                    <div class="cab__site">
+                        <h2>Сайт:</h2>
+                        <input type="text">
+                        </div>
+
+                    <div class="cab__mail">
+                        <h2>Электронная почта</h2>
+                        <input type="text" placeholder="somemail@gmail.com">
+                    </div>
                 </div>
-        </div>
 
-        <div class="mid__mail">
-            <input type="text">
-            <h2>somemail@gmail.com</h2>
-        </div>
 
-        <div class="mid__about">
-            <div class="mAbout__you">
-                <h2>Немного о себе</h2>
-                <input type="text">
-                <h3>Краткий рассказ о себе — кто вы, чем занимаетесь и прочее...
-                    не более 300 символов</h3>
+                <div class="mid__about">
+                    <div class="mAbout__you">
+                        <h2>Немного о себе</h2>
+                        <input type="text">
+                        <h3>Краткий рассказ о себе — кто вы, чем занимаетесь и прочее...
+                            не более 300 символов</h3>
+                    </div>
+
+                    <div class="mAbout__interest">
+                        <h2>Ваши интересы</h2>
+                        <input type="text">
+                        <h3>Перечислите ваши интересы (хобби) через запятую.
+                                например: футбол, ролики, рыбалка, вязание</h3>
+                    </div>
+                </div>
             </div>
+            <div class="cab__right">
+                <div class="cabR__top">
+                    <h2>Пол</h2>
+                    <select name="" id="">
+                        <option>Мужской</option>
+                        <option>Женский</option>
+                    </select>
 
-            <div class="mAbout_interest">
-                <h2>Ваши интересы</h2>
+                    <h2>Дата рождения</h2>
+                    <div class="CRT__date">
+                        <input type="date">
+                        <input type="checkbox">
+                        <label for="checkbox">Скрывать</label>
+                    </div>
+                </div>
+                <h2>Страна</h2>
                 <input type="text">
-                <h3>Перечислите ваши интересы (хобби) через запятую.
-                        например: футбол, ролики, рыбалка, вязание</h3>
+
+                <h2>Город</h2>
+                <input type="text">
+
+                <h2>Вконтакте</h2>
+                <input type="text">
+
+                <h2>Telegram</h2>
+                <input type="text">
+                <button>Сохранить</button>
             </div>
-        </div>
     </div>
-    <div class="cab__right">
-        <div class="cabR__top">
-            <h2>Пол</h2>
-            <select name="" id="">
-                <option>Мужской</option>
-                <option>Женский</option>
-            </select>
-
-            <h2>Дата рождения</h2>
-            <div class="CRT__date">
-                <input type="date">
-                <input type="checkbox">
-                <h2>Не показывать в профиле</h2>
-            </div>
+    <div class="cabinet__buyed">
+        <h1>Приобретенное:</h1>
+        <div class="buyed__list">
+            <img src="./content/serials/9/9h.png" alt="">
+            <img src="./content/serials/1/1h.png" alt="">
+            <img src="./content/films/7/7h.png" alt="">
+            <img src="./content/films/5/5h.png" alt="">
+            <img src="./content/serials/4/4h.png" alt="">
+            <img src="./content/films/3/3h.png" alt="">
         </div>
-        <h2>Страна</h2>
-        <input type="text">
-
-        <h2>Город</h2>
-        <input type="text">
-
-        <h2>Вконтакте</h2>
-        <input type="text">
-
-        <h2>Telegram</h2>
-        <input type="text">
-        <button>Сохранить</button>
     </div>
 </wrapper>
 </body>
